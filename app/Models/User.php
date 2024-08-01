12c46139349d0717035b53b1c19680c542c647e1
Ahmad Hudori <ahmadhudori06@gmail.com>
2024-08-01 15:22:51 +0700

    adding models and relationship on User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Cart;
+use App\Models\Order;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
@@ -20,6 +23,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+	   'is_admin',
     ];
 
     /**
@@ -44,4 +48,14 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+	public function orders():HasMany
+	{
+		return $this->hasMany(Order::class);
+	}
+
+	public function carts():HasMany
+	{
+		return $this->hasMany(Cart::class);
+	}
+
 }
