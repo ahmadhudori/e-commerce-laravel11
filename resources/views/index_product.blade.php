64be1bb27ca00159b3a00032942a633b83174dd4
Ahmad Hudori <ahmadhudori06@gmail.com>
2024-08-14 00:34:08 +0700

    adding style to index product

--- resources/views/index_product.blade.php
+++ resources/views/index_product.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
@@ -21,4 +21,67 @@
 	    <hr>
 	@endforeach
 </body>
-</html>
+</html> --}}
+
+<x-app-layout>
+
+	<!--
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/aspect-ratio'),
+    ],
+  }
+  ```
+-->
+
+<x-slot name="header">
+	<h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+	    {{ __('My Product') }}
+	</h2>
+ </x-slot>
+
+ <x-primary-button class="-mx-auto"><a href="{{ route('create_product') }}">create product</a></x-primary-button>
+
+<div class="bg-gray-100">
+	<div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+	  <h2 class="text-2xl font-bold tracking-tight text-gray-900">Customers also purchased</h2>
+   
+	  <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+		@foreach ($products as $product)
+	    <div class="group relative">
+		 <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+		   <img src="{{ url('storage/' . $product->image) }}" alt="Front of men&#039;s Basic Tee in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+		 </div>
+		 <div class="mt-4 flex justify-between">
+		   <div>
+			<h3 class="text-base font-semibold text-gray-700">
+			  <a href="{{ route('show_product',  $product) }}">
+			    {{ $product->name }}
+			  </a>
+			</h3>
+			<p class="mt-1 text-sm text-gray-400">{{ Str::limit($product->description, 10) }}</p>
+		   </div>
+		   <p class="text-sm font-medium text-gray-900">Rp. {{ Number::format($product->price, locale: 'id') }}</p>
+		 </div>
+		 <div>
+			<form action="{{ route('delete_product', $product) }}" method="post" onsubmit="return confirm('Are you sure?')">
+				@method('delete')
+				@csrf
+				<x-danger-button>Delete</x-danger-button>
+			</form>
+		 </div>
+	    </div>
+	    	@endforeach
+   
+	    <!-- More products... -->
+	  </div>
+	</div>
+   </div>
+   
+</x-app-layout>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,6 +16,11 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('index_product')" :active="request()->routeIs('index_product')">
+                        {{ __('Shop') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
@@ -23,8 +28,10 @@
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+					@if(Auth::user() == null)
+					@else
                             <div>{{ Auth::user()->name }}</div>
-
+					@endif
                             <div class="ms-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
@@ -45,7 +52,12 @@
                             <x-dropdown-link :href="route('logout')"
                                     onclick="event.preventDefault();
                                                 this.closest('form').submit();">
-                                {{ __('Log Out') }}
+                                {{-- {{ __('Log Out') }} --}}
+						  @if(Auth::user() == null)
+						  {{ __('Log In') }}
+						  @else
+						  {{ __('Log Out') }}
+						  @endif
                             </x-dropdown-link>
                         </form>
                     </x-slot>
@@ -75,8 +87,12 @@
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
             <div class="px-4">
-                <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+			@if(Auth::user() == null)
+			@else
+			<div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
+			<div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+			@endif
+                
             </div>
 
             <div class="mt-3 space-y-1">
@@ -91,7 +107,12 @@
                     <x-responsive-nav-link :href="route('logout')"
                             onclick="event.preventDefault();
                                         this.closest('form').submit();">
-                        {{ __('Log Out') }}
+                        {{-- {{ __('Log Out') }} --}}
+				    @if(Auth::user() == null)
+				    {{ __('Log In') }}
+				    @else
+				    {{ __('Log Out') }}
+				    @endif
                     </x-responsive-nav-link>
                 </form>
             </div>
